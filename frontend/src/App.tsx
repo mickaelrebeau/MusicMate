44b25959d2713d5c7ce8f498d8f4093d0dc606b4
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-03 03:34:46 +0200

    🛠️ Adding somes pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
+import { Auth } from "./pages/auth/AuthPage";
+import { SignUp } from "./pages/signup/SignUpPage";
+import { Login } from "./pages/login/LoginPage";
 import { Home } from "./pages/home/HomePage";
-import { Profile } from "./pages/profile/ProfilePage";
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Auth />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   )
