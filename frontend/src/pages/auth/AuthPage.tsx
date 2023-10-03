fe338850aad867082fc3642e29f95e82dd3c0d99
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-03 20:32:15 +0200

    ➕ Cursor Pointer

--- frontend/src/pages/auth/AuthPage.tsx
+++ frontend/src/pages/auth/AuthPage.tsx
@@ -17,7 +17,7 @@ export function Auth() {
                     to MusicMate 
                 </p>
                 <p className="max-w-[800px] mt-8 text-slate-500 text-center text-2xl">Music is the universal language of the world and we want to make it easier for you to discover new music from all over the world.</p>
-                <p className="mt-8 px-10 py-3 rounded-xl text-2xl font-semibold text-indigo-500 border-indigo-500 hover:text-white hover:bg-gradient-to-r from-indigo-600 to-purple-400 hover:shadow-2xl hover:shadow-indigo-500/50">
+                <p className="mt-8 px-10 py-3 rounded-xl text-2xl font-semibold text-indigo-500 border-indigo-500 cursor-pointer hover:text-white hover:bg-gradient-to-r from-indigo-600 to-purple-400 hover:shadow-2xl hover:shadow-indigo-500/50">
                     <NavLink to={"/signup"} >Get Started</NavLink>
                 </p>
                 <p className="mt-8 text-slate-500">
