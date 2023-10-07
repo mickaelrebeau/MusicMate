00bf4b89736579f1150f5c6982fd70badf5b3ca5
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-07 04:59:51 +0200

    ♻️ refacto: rebase routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,66 @@
 import './App.css';
-import { Route, Routes } from "react-router-dom";
-import { Auth } from "./pages/auth/AuthPage";
-import { SignUp } from "./pages/signup/SignUpPage";
-import { Login } from "./pages/login/LoginPage";
-import { Home } from "./pages/home/HomePage";
-import { Sidebar } from "./components/sidebar";
+import { Outlet, Route, Routes, useLocation } from 'react-router-dom';
+import { Hero } from './pages/auth/HeroPage';
+import { SignUp } from './pages/auth/SignUpPage';
+import { Login } from './pages/auth/LoginPage';
+import { Home } from './pages/home/HomePage';
+import { Sidebar } from './components/sidebar';
+import { SearchPage } from './pages/home/SearchPage';
+import { Playlists } from './pages/playlists/Playlists';
 
 function App() {
+   return (
+     <Routes>
+       <Route path="/" element={<Layout />}>
+         {/* Auth pages */}
+         <Route index element={<Hero />} />
+         <Route path="signup" element={<SignUp />} />
+         <Route path="login" element={<Login />} />
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-        
-      <div className="flex">
-        <Sidebar />
-        <Routes>
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>
-    </>
-  )
+         {/* Website pages */}
+         <Route path="home" element={<Home />} />
+         <Route path="songs" element={<>Songs</>} />
+         <Route path="search" element={<SearchPage />} />
+         <Route path="artists" element={<>Artist</>} />
+         <Route path="albums" element={<>Albums</>} />
+
+         {/* Playlists pages */}
+         <Route path="/playlists">
+           <Route index element={<Playlists />} />
+           <Route
+             path="recently-added"
+             element={<>Playlists recently added</>}
+           />
+           <Route
+             path="recently-played"
+             element={<>Playlists recently played</>}
+           />
+
+           {/* loader pour récuperer les datas des playlits */}
+           <Route
+             path=":slug"
+             element={<>My playlist 1</>}
+             loader={() => ['hello', 'world']}
+           />
+         </Route>
+       </Route>
+     </Routes>
+   );
 }
 
-export default App
+function Layout() {
+   const { pathname } = useLocation();
+   const authPages = ['/', '/signup', '/login'];
+   const isAuthPage = authPages.includes(pathname);
+
+   if (isAuthPage) return <Outlet />;
+
+   return (
+      <div className="flex">
+         <Sidebar />
+         <Outlet />
+      </div>
+   );
+}
+
+export default App;
