1ffc5a6ad5a2d6ea6f834f365870a1c9201cdf56
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-07 05:06:10 +0200

    ♻️ refacto: rebase routing

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,53 +1,89 @@
-import { Library, ListMusic, Mic2, Music2, PlayCircle } from "lucide-react";
+import {
+  Library,
+  ListMusic,
+  LucideIcon,
+  Mic2,
+  Music2,
+  PlayCircle,
+  Search,
+} from 'lucide-react';
+import { cn } from '../libs/tailwind';
+import { NavLink } from 'react-router-dom';
 
 export function Sidebar() {
-    return (
-        <>
-            <div className="flex flex-col px-3 py-6 shadow w-80 border-r-2 border-gray-700">
-                <div className="flex flex-col gap-10">
-                    <div className="flex flex-col gap-6">
-                        <h2 className="text-xl font-bold">Discover</h2>
-                        <div className="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-600 rounded">
-                            <PlayCircle size={20} />
-                            <span>Listen Now</span>
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-4">
-                        <h2 className="mb-4 text-xl font-bold">Library</h2>
-                        <div className="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
-                            <ListMusic size={20} />
-                            <span>Playlists</span>
-                        </div>
-                        <div className="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
-                            <Music2 size={20} />
-                            <span>Songs</span>
-                        </div>
-                        <div className="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
-                            <Mic2 size={20} />
-                            <span>Artists</span>
-                        </div>
-                        <div className="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
-                            <Library size={20} />
-                            <span>Albums</span>
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-4">
-                        <h2 className="mb-4 text-xl font-bold">Playlists</h2>
-                        <div className="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
-                            <ListMusic size={20} />
-                            <span>Recently Added</span>
-                        </div>
-                        <div className="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
-                            <ListMusic size={20} />
-                            <span>Recently Played</span>
-                        </div>
-                        <div className="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
-                            <ListMusic size={20} />
-                            <span>My Playlist 1</span>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="flex flex-col px-3 py-6 shadow w-72 border-r border-gray-700">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl font-bold">Discover</h2>
+            <MenuItem link="/home" icon={PlayCircle} text="Listen Now" />
+            <MenuItem link="/search" icon={Search} text="Search" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl font-bold">Library</h2>
+            <MenuItem link="/playlists" icon={ListMusic} text="Playlists" />
+            <MenuItem link="/songs" icon={Music2} text="Songs" />
+            <MenuItem link="/artists" icon={Mic2} text="Artists" />
+            <MenuItem link="/albums" icon={Library} text="Albums" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl font-bold">Playlists</h2>
+            <div className="h-[150px] 2xl:h-full overflow-auto">
+              <MenuItem
+                link="/playlists/recently-added"
+                icon={ListMusic}
+                text="Recently Added"
+              />
+              <MenuItem
+                link="/playlists/recently-played"
+                icon={ListMusic}
+                text="Recently Played"
+              />
+              <MenuItem
+                link="/playlists/my-playlist-1"
+                icon={ListMusic}
+                text="My Playlist 1"
+              />
+              <MenuItem
+                link="/playlists/my-playlist-2"
+                icon={ListMusic}
+                text="My Playlist 2"
+              />
+              <MenuItem
+                link="/playlists/my-playlist-3"
+                icon={ListMusic}
+                text="My Playlist 3"
+              />
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+type MenuItemProps = {
+  icon: LucideIcon;
+  text: string;
+  className?: string;
+  link: string;
+};
+
+function MenuItem({ icon: Icon, text, className, link }: MenuItemProps) {
+  return (
+    <NavLink
+      to={link}
+      end
+      className={({ isActive }) =>
+        cn(
+          'flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded',
+          className,
+          { 'bg-purple-500': isActive }
+        )
+      }>
+      <Icon size={20} />
+      <span>{text}</span>
+    </NavLink>
+  );
 }
