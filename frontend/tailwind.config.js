301bd7c58d10d2908d56be342fa39c3b1c7d54bf
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-07 04:56:31 +0200

    💄style: add prettier

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,28 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        fadeIn: {
-          '0%': {
-            opacity: '0',
-          },
-          '100%': {
-            opacity: '1',
-          },
-        }
+   content: [
+      './pages/**/*.{ts,tsx}',
+      './components/**/*.{ts,tsx}',
+      './app/**/*.{ts,tsx}',
+      './src/**/*.{ts,tsx}',
+   ],
+   theme: {
+      extend: {
+         keyframes: {
+            fadeIn: {
+               '0%': {
+                  opacity: '0',
+               },
+               '100%': {
+                  opacity: '1',
+               },
+            },
+         },
+         animation: {
+            fadeIn: 'fadeIn 1s ease-in-out',
+         },
       },
-      animation: {
-        'fadeIn': 'fadeIn 1s ease-in-out'
-      }
-    },
-  },
-  plugins: [],
-}
-
+   },
+   plugins: [],
+};
