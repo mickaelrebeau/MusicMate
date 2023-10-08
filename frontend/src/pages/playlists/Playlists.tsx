e65935c6891e644d4658ba14ad0fa4627e6ea629
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-08 04:47:37 +0200

    ♻️ refacto: add modal button

--- frontend/src/pages/playlists/Playlists.tsx
+++ frontend/src/pages/playlists/Playlists.tsx
@@ -1,5 +1,6 @@
 import { DailyMixCard } from '@/src/components/dailyMixCard';
-import { MoreHorizontal, Plus } from 'lucide-react';
+import { ModalNewPlaylist } from '@/src/components/modals/ModalNewPlaylist';
+import { MoreHorizontal } from 'lucide-react';
 
 export function Playlists() {
   const playlistsNumber = 10;
@@ -7,13 +8,10 @@ export function Playlists() {
     'https://upload.wikimedia.org/wikipedia/en/1/11/Dive_tycho_album.jpg';
 
   return (
-    <section className="px-6 py-10 mx-auto flex flex-col gap-10">
+    <section className="px-6 py-10 mx-auto flex flex-col">
       <div className="flex items-center justify-between gap-10">
         <h2 className="text-xl font-bold">Playlists</h2>
-        <button className="flex items-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
-          <Plus size={30} />
-          add a new playlist
-        </button>
+        <ModalNewPlaylist />
       </div>
       <div className="mt-6 grid grid-cols-3 xl:grid-cols-4 min-[1800px]:grid-cols-5 gap-6">
         {[...Array(playlistsNumber)].map((_, i) => (
