4b5520d8c5a8679c85656996655398f8531918b7
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-09 02:55:15 +0200

    🛠️ app: add Sportify API  connexion call

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,28 @@
 import { DailyMixCard } from '@/src/components/dailyMixCard';
 import { ListPlus, Shuffle } from 'lucide-react';
+import { useEffect } from 'react';
 
 export function Home() {
+  const clientID = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
   const playlistsNumber = 5;
   const url =
     'https://upload.wikimedia.org/wikipedia/en/1/11/Dive_tycho_album.jpg';
+
+  useEffect(() => {
+    const authParams = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`,
+    };
+    fetch('https://accounts.spotify.com/api/token', authParams)
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem('token', data.access_token);
+      });
+  }, [clientID, clientSecret]);
 
   return (
     <section className="p-10 mx-auto h-screen flex flex-col gap-10">
@@ -14,7 +32,8 @@ export function Home() {
           Listen to new music from all over the world. Click the shuffle button
           to listen to a random song.
         </p>
-        <button className="w-[200px] flex items-center justify-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
+        <button
+          className="w-[200px] flex items-center justify-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
           <Shuffle size={20} />
           Shuffle Playback
         </button>
@@ -24,9 +43,9 @@ export function Home() {
         <p className="text-slate-500">
           Your personal playlists. Updated daily.
         </p>
-        <div className="mt-6 flex gap-6">
+        <div className="mt-6 grid grid-cols-3 xl:grid-cols-4 min-[1800px]:grid-cols-5 gap-6">
           {Array.from({ length: playlistsNumber }).map((_, i) => (
-           <DailyMixCard
+            <DailyMixCard
               key={i}
               playlistName={'Daily Playlist ' + (i + 1)}
               imageUrl={url}
