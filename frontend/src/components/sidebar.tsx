0ff08535d24fe6bcd57a20101152137952fa8370
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-11 15:59:09 +0200

    ♻️ refacto: remove somes route

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,6 +1,4 @@
 import {
-  Library,
-  ListMusic,
   LucideIcon,
   Mic2,
   Music2,
@@ -13,52 +11,20 @@ import { NavLink } from 'react-router-dom';
 export function Sidebar() {
   return (
     <>
-      <div className="flex flex-col px-3 py-6 shadow w-72 border-r border-gray-700">
-        <div className="flex flex-col gap-6 sticky top-0">
-          <div className="flex flex-col gap-4">
+      <section className="flex flex-col px-3 pt-6 shadow w-72 border-r border-gray-700">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
             <h2 className="text-xl font-bold">Discover</h2>
             <MenuItem link="/home" icon={PlayCircle} text="Listen Now" />
             <MenuItem link="/search" icon={Search} text="Search" />
           </div>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
             <h2 className="text-xl font-bold">Library</h2>
-            <MenuItem link="/playlists" icon={ListMusic} text="Playlists" />
             <MenuItem link="/songs" icon={Music2} text="Songs" />
             <MenuItem link="/artists" icon={Mic2} text="Artists" />
-            <MenuItem link="/albums" icon={Library} text="Albums" />
-          </div>
-          <div className="flex flex-col gap-4">
-            <h2 className="text-xl font-bold">Playlists</h2>
-            <div className="h-[150px]  overflow-y-scroll">
-              <MenuItem
-                link="/playlists/recently-added"
-                icon={ListMusic}
-                text="Recently Added"
-              />
-              <MenuItem
-                link="/playlists/recently-played"
-                icon={ListMusic}
-                text="Recently Played"
-              />
-              <MenuItem
-                link="/playlists/my-playlist-1"
-                icon={ListMusic}
-                text="My Playlist 1"
-              />
-              <MenuItem
-                link="/playlists/my-playlist-2"
-                icon={ListMusic}
-                text="My Playlist 2"
-              />
-              <MenuItem
-                link="/playlists/my-playlist-3"
-                icon={ListMusic}
-                text="My Playlist 3"
-              />
-            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
