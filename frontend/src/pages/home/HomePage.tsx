23f94853f20bd15dc264f9c106a198d2b5715054
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-11 15:59:48 +0200

    💄 style: change style

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -24,25 +24,24 @@ export function Home() {
   }, []);
 
   return (
-    <section className="p-10 mx-auto h-screen flex flex-col gap-10">
-      <div className="flex flex-col gap-4">
+    <section className="px-10 pt-10 pb-20 mx-auto flex flex-col gap-6">
+      <div className="flex flex-col gap-2">
         <h2 className="text-2xl font-bold">Listen Now</h2>
         <p className="text-slate-500">
           Listen to new music from all over the world. Click the shuffle button
           to listen to a random song.
         </p>
-        <button
-          className="w-[200px] flex items-center justify-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
+        <button className="w-[200px] flex items-center justify-center gap-2 p-2 font-semibold cursor-pointer hover:bg-purple-900 rounded">
           <Shuffle size={20} />
           Shuffle Playback
         </button>
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
         <h2 className="text-2xl font-bold">Made for You</h2>
         <p className="text-slate-500">
           Your personal playlists. Updated daily.
         </p>
-        <div className="mt-6 grid grid-cols-3 xl:grid-cols-4 min-[1800px]:grid-cols-5 gap-6">
+        <div className="mt-2 grid grid-col-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
           {Array.from({ length: playlistsNumber }).map((_, i) => (
             <DailyMixCard
               key={i}
