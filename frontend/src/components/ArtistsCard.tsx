8552e0c271ba2d609031aef807ff7f4ceeb814a0
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-11 15:58:31 +0200

    ♻️ refacto: add total tracks and link to spotify

--- frontend/src/components/ArtistsCard.tsx
+++ frontend/src/components/ArtistsCard.tsx
@@ -2,10 +2,14 @@ export function ArtistsCard({
   url,
   albumName,
   artistName,
+  totalTracks,
+  href,
 }: {
   url: string;
   albumName: string;
   artistName: string;
+  totalTracks: number;
+  href: string;
 }) {
   return (
     <div className="max-w-[240px] p-4 flex flex-col items-center gap-2 border rounded-lg">
@@ -17,6 +21,10 @@ export function ArtistsCard({
       <div className="p-2 w-full">
         <h2 className="text-left font-bold">{albumName}</h2>
         <p className="text-left text-slate-500">{artistName}</p>
+        <p className="text-left text-slate-500">{totalTracks} songs.</p>
+        <a href={href} target="_blank" className="pt-8 hover:text-purple-500">
+          View on Spotify
+        </a>
       </div>
     </div>
   );
