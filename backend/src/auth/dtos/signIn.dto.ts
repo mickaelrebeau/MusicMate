e6cbe0c98a8e654a0a8adf3b7dd5ec873518954d
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-15 12:43:47 +0200

    🛠️ app: add login exemple to swagger api

--- backend/src/auth/dtos/signIn.dto.ts
+++ backend/src/auth/dtos/signIn.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class SignInDto {
-    @ApiProperty({ example: "test@test.fr", description: 'User email' })
+    @ApiProperty({ example: "mike@admin.fr", description: 'User email' })
     email: string;
 
-    @ApiProperty({ example: "test", description: 'User password' })
+    @ApiProperty({ example: "mickaeltest", description: 'User password' })
     password: string;
 }
