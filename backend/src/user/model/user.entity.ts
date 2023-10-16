68bd514e785550848fd1fa911b99ed3d7c404ea0
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-16 15:38:15 +0200

    ♻️ refacto: remove password select

--- backend/src/user/model/user.entity.ts
+++ backend/src/user/model/user.entity.ts
@@ -12,7 +12,7 @@ export class User {
   pseudo: string;
 
   @ApiProperty({ example: 'miketest', description: 'User password' })
-  @Column()
+  @Column({ select: false })
   password: string;
 
   @ApiProperty({ example: 'mike@admin.fr', description: 'User email' })
