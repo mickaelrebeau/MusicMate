e0c65cba225500142ecddb4c53542995176a5eb5
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-16 15:36:21 +0200

    ♻️ refacto: update profil pages

--- frontend/src/pages/profil/EditProfilPage.tsx
+++ frontend/src/pages/profil/EditProfilPage.tsx
@@ -1,22 +1,50 @@
+import { authorizationProfil } from '@/src/services/user';
 import { musicGenre } from '@/src/utils/genres';
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export function EditProfil() {
+  const [user, setUser] = useState<{
+    pseudo: string;
+    email: string;
+    genres: string[];
+  }>({
+    pseudo: '',
+    email: '',
+    genres: [],
+  });
+
+  useEffect(() => {
+    authorizationProfil().then((response) => {
+      setUser({
+        pseudo: response.pseudo,
+        email: response.email,
+        genres: response.genres,
+      });
+    });
+  }, []);
+  
   return (
     <form className="px-10 pt-10 pb-20 mx-auto flex flex-col items-center gap-6">
       <div className="flex flex-col gap-4">
         <h2 className="text-2xl font-bold text-center">Profil</h2>
         <div className="w-[400px] flex flex-col gap-2">
           <label htmlFor="pseudo">Pseudo</label>
-          <input id="pseudo" className="rounded p-2" type="text" />
+          <input
+            id="pseudo"
+            className="rounded p-2"
+            type="text"
+            defaultValue={user.pseudo}
+          />
         </div>
         <div className="w-[400px] flex flex-col gap-2">
           <label htmlFor="email">Email</label>
-          <input id="email" className="rounded p-2" type="email" />
-        </div>
-        <div className="w-[400px] flex flex-col gap-2">
-          <label htmlFor="password">Password</label>
-          <input id="password" className="rounded p-2" type="password" />
+          <input
+            id="email"
+            className="rounded p-2"
+            type="email"
+            defaultValue={user.email}
+          />
         </div>
       </div>
       <div className="flex flex-col gap-2 mt-8">
@@ -31,6 +59,7 @@ export function EditProfil() {
                 id={genre}
                 value={genre}
                 className="hidden"
+                checked={user.genres.includes(genre)}
               />
               <label
                 htmlFor={genre}
--- frontend/src/pages/profil/ProfilPage.tsx
+++ frontend/src/pages/profil/ProfilPage.tsx
@@ -1,39 +1,40 @@
-// import { api } from '@/src/libs/axios';
-import { musicGenre } from '@/src/utils/genres';
+import { authorizationProfil } from '@/src/services/user';
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-// import { useEffect, useState } from 'react';
 
 export function Profil() {
-  // const userId = localStorage.getItem('userId');
-  // const [user, setUser] = useState({
-  //   pseudo: '',
-  //   email: '',
-  //   password: '',
-  //   genres: [],
-  // })
+  const [user, setUser] = useState({
+    pseudo: '',
+    email: '',
+    genres: [],
+  })
 
-  // useEffect(() => {
-  //   api.get('/user', userId)
-  //     .then((response) => {
-  //       setUser(response.data)
-  //     })
-  // }, [userId])
+  useEffect(() => {
+    authorizationProfil()
+    .then((response) => {
+      setUser({
+        pseudo: response.pseudo,
+        email: response.email,
+        genres: response.genres
+      })
+    })
+  }, [])
 
   return (
     <section className="px-10 pt-10 pb-20 mx-auto flex flex-col items-center justify-center gap-10">
       <div className="flex flex-col gap-4">
         <h2 className="text-2xl text-center font-bold">Profil</h2>
         <p className="py-2 px-10 border border-slate-600 rounded shadow shadow-purple-500/30">
-          Mike_dreeman
+          {user.pseudo}
         </p>
         <p className="py-2 px-10 border border-slate-600 rounded shadow shadow-purple-500/30">
-          mike@admin.fr
+          {user.email}
         </p>
       </div>
       <div className="flex flex-col gap-4">
         <h2 className="text-2xl text-center font-bold">Genres</h2>
         <div className="grid grid-cols-4 gap-2 ">
-          {musicGenre.map((genre, index) => (
+          {user.genres.map((genre, index) => (
             <p
               key={index}
               className="p-2 text-center border border-slate-600 rounded shadow shadow-purple-500/30">
