c5aa51f74718662333c8c4c08b057c707320d5de
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-22 19:35:04 +0200

    ♻️ refacto: update edit user page

--- frontend/src/pages/profil/EditProfilPage.tsx
+++ frontend/src/pages/profil/EditProfilPage.tsx
@@ -1,14 +1,27 @@
-import { authorizationProfil } from '@/src/services/user';
+import { authorizationProfil, updateProfil } from '@/src/services/user';
 import { musicGenre } from '@/src/utils/genres';
 import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import * as z from 'zod';
+
+const schema = z.object({
+  pseudo: z
+    .string()
+    .min(3, { message: 'Must be 3 or more characters long' })
+    .max(20, { message: 'Must be 20 or fewer characters long' })
+    .refine((value) => /^[a-zA-Z0-9]+$/.test(value), {
+      message: 'Pseudo can only contain letters and numbers',
+    }),
+  email: z.string().email({ message: 'Invalid email address' }),
+  genres: z.array(z.string()),
+});
+
+type User = z.infer<typeof schema>;
 
 export function EditProfil() {
-  const [user, setUser] = useState<{
-    pseudo: string;
-    email: string;
-    genres: string[];
-  }>({
+  const navigate = useNavigate();
+  const userId = localStorage.getItem('userId');
+  const [user, setUser] = useState<User>({
     pseudo: '',
     email: '',
     genres: [],
@@ -23,17 +36,59 @@ export function EditProfil() {
       });
     });
   }, []);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await updateProfil(user, userId as string);
+      navigate('/profil');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({
+      ...user,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  }
+
+  const handleChangeGenre = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = e.currentTarget;
+    if (checked) {
+      setUser((prevUser) => {
+        return {
+          ...prevUser,
+          genres: [...prevUser.genres, value],
+        };
+      });
+    } else {
+      setUser((prevUser) => {
+        return {
+          ...prevUser,
+          genres: prevUser.genres.filter((genre) => genre !== value),
+        };
+      });
+    }
+    console.log(user.genres);
+  };
   
   return (
-    <form className="px-10 pt-10 pb-20 mx-auto flex flex-col items-center gap-6">
+    <form
+      onSubmit={handleSubmit}
+      className="px-10 pt-10 pb-20 mx-auto flex flex-col items-center gap-6">
       <div className="flex flex-col gap-4">
         <h2 className="text-2xl font-bold text-center">Profil</h2>
         <div className="w-[400px] flex flex-col gap-2">
           <label htmlFor="pseudo">Pseudo</label>
           <input
             id="pseudo"
+            name='pseudo'
             className="rounded p-2"
             type="text"
+            onChange={handleChange}
             defaultValue={user.pseudo}
           />
         </div>
@@ -41,8 +96,10 @@ export function EditProfil() {
           <label htmlFor="email">Email</label>
           <input
             id="email"
+            name='email'
             className="rounded p-2"
             type="email"
+            onChange={handleChange}
             defaultValue={user.email}
           />
         </div>
@@ -58,12 +115,16 @@ export function EditProfil() {
                 type="checkbox"
                 id={genre}
                 value={genre}
+                onChange={handleChangeGenre}
                 className="hidden"
-                checked={user.genres.includes(genre)}
               />
               <label
                 htmlFor={genre}
-                className="w-[200px] text-center font-semibold cursor-pointer py-1 rounded hover:bg-purple-950">
+                className={`w-[200px] text-center font-semibold cursor-pointer py-1 rounded hover:bg-purple-950 ${
+                  user.genres.includes(genre)
+                    ? 'text-white bg-purple-800'
+                    : 'text-slate-400'
+                }`}>
                 {genre}
               </label>
             </div>
