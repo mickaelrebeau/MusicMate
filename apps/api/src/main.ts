bc7ade3967fc621a87f821dd4c94fce842d767a3
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-24 14:29:07 +0200

    🛠️ app: update api main

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cors from 'cors';
+import * as dotenv  from 'dotenv';
 
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
