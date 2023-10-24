79da62188648ce6c201038a38978cd1eb60b513f
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-24 12:36:21 +0200

    🛠️ app: update api

--- apps/client/src/libs/axios.ts
+++ apps/client/src/libs/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 const headers = { 'Access-Control-Allow-Origin': '*' };
 
 export const api = axios.create({
-   baseURL: 'http://localhost:3030/',
+   baseURL: 'http://localhost:3030/api',
    headers,
 });
