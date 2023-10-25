91c5577cd1517103d814a6a7c24fe13444d10096
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-26 01:09:30 +0200

    🛠️ app: update api request

--- apps/client/src/libs/axios.ts
+++ apps/client/src/libs/axios.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-const headers = {
-  'Access-Control-Allow-Origin': 'https://musicmate.vercel.app',
-};
+// const headers = {
+//   'Access-Control-Allow-Origin': 'https://musicmate.vercel.app',
+// };
 
 export const api = axios.create({
   // baseURL: 'http://localhost:3030/api',
   baseURL: 'https://musicmate-api-production.up.railway.app/api',
-  headers,
 });
