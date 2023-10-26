99a17a936e70961e265c2a08a8131bea2e6a8776
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-26 17:46:10 +0200

    🛠️ app: update cors interceptor

--- apps/api/src/intercerptors/CorsInterceptor.ts
+++ apps/api/src/intercerptors/CorsInterceptor.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 export class CorsInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     const response = context.switchToHttp().getResponse();
-    response.header('Access-Control-Allow-Origin', 'https://musicmate.vercel.app');
+    response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Headers', 'Content-Type');
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     response.header('Access-Control-Allow-Credentials', 'true');
