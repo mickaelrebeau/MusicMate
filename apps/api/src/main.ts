0249fd5d00bcea72d72d9ed98004ccb88ea1d3c9
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-26 18:17:59 +0200

    🛠️ app: update api request

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,17 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cors from 'cors';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:8080', 'https://musicmate.vercel.app'],
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
-    credentials: true,
-  });
+  app.use(
+    cors({
+      origin: ['http://localhost:8080', 'https://musicmate.vercel.app'],
+      methods: ['GET', 'PUT', 'POST', 'DELETE'],
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    })
+  )
 
   const config = new DocumentBuilder()
     .setTitle('Global example')
