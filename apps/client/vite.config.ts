29adea55b90331c0ba39bcf6c8195cc09e8a8ef1
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-28 18:07:23 +0200

    🛠️ app: update vite

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,19 +3,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   plugins: [react()],
-   resolve: {
-      alias: {
-         '@': path.resolve(__dirname, '.'),
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        // target: 'http://localhost:3030',
+        target: 'https://musicmate.up.railway.app/',
+        changeOrigin: true,
       },
-   },
-   server: {
-      proxy: {
-         '/api': {
-            target: 'http://localhost:3030',
-            changeOrigin: true,
-         },
-      },
-      port: 8080,
-   },
+    },
+    port: 8080,
+  },
 });
